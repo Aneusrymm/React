b2de0decef9ff80be7ef10f915c5dcca0f353896
Nabil Firdaus <nabilfirdaus999@gmail.com>
2025-07-10 15:42:41 +0700

    Add actual content of React-18 project

---
+++ React-18/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+<script src="http://192.165.5.39:8097"></script>
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board(){
+  return (
+    <>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    </>
+  );
+}
